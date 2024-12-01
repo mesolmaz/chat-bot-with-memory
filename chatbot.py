1bc54f8e756f3d284662c15e020a03f922731800
Solmaz <mehmet.solmaz@intel.com>
2024-12-01 09:45:14 -0800

    first commit

---
+++ chatbot.py
@@ -0,0 +1,76 @@
+import uuid
+import asyncio
+from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain_core.messages import AIMessage
+from langchain_core.runnables import RunnableConfig, RunnableLambda, RunnableSerializable
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, END, MessagesState, StateGraph
+from langchain_openai import ChatOpenAI
+
+model = ChatOpenAI(model="gpt-4o-mini")
+
+# Define the function that calls the model
+def call_model(state: MessagesState):
+    system_prompt = (
+        "You are a helpful assistant. "
+        "Answer all questions to the best of your ability."
+    )
+    messages = [SystemMessage(content=system_prompt)] + state["messages"]
+    response = model.invoke(messages)
+    return {"messages": response}
+
+# # Define the function that calls the model
+# async def call_model(state: MessagesState):
+#     system_prompt = (
+#         "You are a helpful assistant. "
+#         "Answer all questions to the best of your ability."
+#     )
+#     messages = [SystemMessage(content=system_prompt)] + state["messages"]
+#     response = await model.ainvoke(messages)
+#     return {"messages": response}
+
+# Define a new graph
+workflow = StateGraph(state_schema=MessagesState)
+# Define the two nodes we will cycle between
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+chatbot = workflow.compile(
+    checkpointer=MemorySaver()
+)
+
+# The thread id is a unique key that identifies
+# this particular conversation.
+# We'll just generate a random uuid here.
+# This enables a single application to manage conversations among multiple users.
+# thread_id = uuid.uuid4()
+# config = {"configurable": {"thread_id": thread_id}}
+
+# input_message = HumanMessage(content="hi! I'm bob")
+# for event in chatbot.stream({"messages": [input_message]}, config, stream_mode="values"):
+#     event["messages"][-1].pretty_print()
+
+# # # Here, let's confirm that the AI remembers our name!
+# # input_message = HumanMessage(content="what was my name?")
+# # for event in chatbot.stream({"messages": [input_message]}, config, stream_mode="values"):
+# #     event["messages"][-1].pretty_print()
+
+# thread_id = uuid.uuid4()
+# config = {"configurable": {"thread_id": thread_id}}
+# print(chatbot.invoke(
+#     {"messages": [HumanMessage(content="Hi I am Mehmet.")]},
+#     config=config))
+
+# print(chatbot.invoke(
+#     {"messages": [HumanMessage(content="what was my name?")]},
+#     config=config))
+
+# thread_id = uuid.uuid4()
+# config = {"configurable": {"thread_id": thread_id}}
+# response = asyncio.run(chatbot.ainvoke({"messages": [HumanMessage(content="Hi I am Mehmet.")]}, config=config))
+# print(response)
+
+# print(chatbot.ainvoke(
+#     {"messages": [HumanMessage(content="what was my name?")]},
+#     config=config))
---
+++ streamlit_app.py
@@ -0,0 +1,59 @@
+import asyncio
+import time
+import uuid
+from pydantic import BaseModel, Field
+import streamlit as st
+from chatbot import chatbot
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+
+st.title("Simple chat-bot")
+
+def response_generator(prompt, config):
+    response = chatbot.invoke({"messages": [HumanMessage(content=prompt)]}, config=config)
+    for word in response['messages'][-1].content.split():
+        yield word + " "
+        time.sleep(0.05)
+
+async def async_response_generator(prompt, config):
+    response = chatbot.ainvoke({"messages": [HumanMessage(content=prompt)]}, config=config)
+    for word in response['messages'][-1].content.split():
+        yield word + " "
+        time.sleep(0.05)
+
+def main() -> None:
+    if "thread_id" not in st.session_state:
+        thread_id = st.query_params.get("thread_id")
+        if not thread_id:
+            thread_id = uuid.uuid4()
+            config = {"configurable": {"thread_id": thread_id}}
+            messages = []
+        st.session_state.messages = messages
+        st.session_state.thread_id = thread_id
+        st.session_state.config = config
+        print(st.session_state.thread_id)
+        print(st.session_state.config)
+
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["type"]):
+            st.markdown(message["content"])
+
+    # Accept user input
+    if prompt := st.chat_input("What is up?"):
+        # Add user message to chat history
+        st.session_state.messages.append({"type": "user", "content": prompt})
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            response = st.write_stream(response_generator(prompt, st.session_state.config))
+        # Add assistant response to chat history
+        st.session_state.messages.append({"type": "assistant", "content": response})
+
+
+if __name__ == "__main__":
+    # asyncio.run(main())
+    main()
